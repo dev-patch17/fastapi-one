53c4cc37561f9722a9007fe517b43a047acc6317
Benjamin Patch <ben@benjaminpatch.com>
2025-05-05 15:35:37 -0700

    Upgrade to async paths

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ class Item(BaseModel):
   is_offer: Union[bool, None] = None
 
 @app.get('/')
-def read_root():
+async def read_root():
   return {'message': 'Hello World!'}
 
 @app.get('/items/{item_id}')
-def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None):
   return {'item_id': item_id, 'q': q}
 
 @app.put('/items/{item_id}')
-def update_item(item_id: int, item: Item):
+async def update_item(item_id: int, item: Item):
   return {'item_name': item.name, 'item_id': item_id}
