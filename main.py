54f44e0256b6c770e950cacd99dc2c64215b32fd
Benjamin Patch <ben@benjaminpatch.com>
2025-05-10 15:31:06 -0700

    Rewrite 4-space indents

--- main.py
+++ main.py
@@ -5,18 +5,21 @@ from pydantic import BaseModel
 app = FastAPI()
 
 class Item(BaseModel):
-  name: str
-  price: float
-  is_offer: Union[bool, None] = None
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
 
 @app.get('/')
 async def read_root():
-  return {'message': 'Hello World!'}
+    return {'message': 'Hello World!'}
+
 
 @app.get('/items/{item_id}')
 async def read_item(item_id: int, q: Union[str, None] = None):
-  return {'item_id': item_id, 'q': q}
+    return {'item_id': item_id, 'q': q}
+
 
 @app.put('/items/{item_id}')
 async def update_item(item_id: int, item: Item):
-  return {'item_name': item.name, 'item_id': item_id}
+    return {'item_name': item.name, 'item_id': item_id}
